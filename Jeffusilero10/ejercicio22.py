76e111df9af5bc2e8fc5f15ba6f0387c5fb7b620
Jeffusilero <107426335+Jeffusilero@users.noreply.github.com>
2023-11-21 22:11:55 -0500

    Add files via upload

---
+++ Jeffusilero10/ejercicio22.py
@@ -0,0 +1,3 @@
+"""Se ingresan por teclado tres números, si todos los valores 
+ingresados son menores a 10, imprimir en pantalla la leyenda 
+"Todos los números son menores a diez"."""
---
+++ Jeffusilero10/ejercicio23.py
@@ -0,0 +1,3 @@
+"""Se ingresan por teclado tres números, si al menos uno de los 
+valores ingresados es menor a 10, imprimir en pantalla la leyenda 
+"Alguno de los números es menor a diez". """
