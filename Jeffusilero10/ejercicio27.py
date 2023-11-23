781d89588e879503b810fec40f80e358bb2b6595
Jeffusilero <107426335+Jeffusilero@users.noreply.github.com>
2023-11-23 11:31:04 -0500

    Delete Jeffusilero10 directory

--- Jeffusilero10/ejercicio27.py
+++
@@ -1,3 +0,0 @@
-""" Escribir un programa en el cual: dada una lista de tres valores 
-numéricos distintos se calcule e informe su rango de variación 
-(debe mostrar el mayor y el menor de ellos) """
