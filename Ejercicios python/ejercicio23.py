77d7bee3c50a0ce1e23dd384b3f51c764e1709c3
Jeffusilero <107426335+Jeffusilero@users.noreply.github.com>
2023-11-24 18:03:20 -0500

    Add files via upload

---
+++ Ejercicios python/ejercicio23.py
@@ -0,0 +1,9 @@
+"""Se ingresan por teclado tres números, si al menos uno de los 
+valores ingresados es menor a 10, imprimir en pantalla la leyenda 
+"Alguno de los números es menor a diez". """
+
+n1= int(input("numero 1 "))
+n2= int(input("numero 2 "))
+n3= int(input("numero 3 "))
+
+if 
